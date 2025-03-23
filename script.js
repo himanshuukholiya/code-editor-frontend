5d7d5e9d210a905104ac5393e04737bd34aaf9ae
himanshuukholiya <kholiyahk14@gmail.com>
2025-03-23 11:56:47 +0530

    code editor layout

---
+++ script.js
@@ -0,0 +1,30 @@
+function updateOutput() {
+    let htmlCode = document.getElementById("html-code").value;
+    let cssCode = document.getElementById("css-code").value;
+    let jsCode = document.getElementById("js-code").value;
+
+    let output = document.getElementById("output").contentDocument;
+
+    /* 
+    output.contentDocument.body.innerHTML = htmlCode + "<style>" + cssCode + "</style>";
+
+    output.contentWindow.eval(jsCode);
+     */
+
+
+    // Clear the iframe's content before writing
+    output.open(); 
+    output.write(`
+        <html>
+            <head>
+                <style>${cssCode}</style>
+            </head>
+            <body>
+                ${htmlCode}
+                <script>${jsCode}<\/script>
+            </body>
+        </html>
+    `);
+    output.close();
+    
+}
